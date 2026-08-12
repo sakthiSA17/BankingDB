4252ba71ba762380efc6e47b69bab32f57c221b4
sakthiSA17 <sakthisingh2005@gmail.com>
2026-08-12 19:47:52 +0530

    first day updated

---
+++ bankingDB_day1.sql
@@ -0,0 +1,11 @@
+create database BankingDB;
+use BankingDB;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+desc customers;
