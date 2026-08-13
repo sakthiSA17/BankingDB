3ae18d86bb626dce620216265987dc5e631a9755
sakthiSA17 <sakthisingh2005@gmail.com>
2026-08-13 23:36:15 +0530

    updated day 1

--- bankingDB_day1.sql
+++ bankingDB_day1.sql
@@ -1,5 +1,7 @@
 create database BankingDB;
+
 use BankingDB;
+
 CREATE TABLE Customers (
     CustomerID INT PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -8,4 +10,79 @@ CREATE TABLE Customers (
     Phone VARCHAR(15),
     AccountCreationDate DATE
 );
+
 desc customers;
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+alter table customers
+Add DateOfBirth date;
+-- Transactions
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+-- Branches
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+-- AccountBranches
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+-- loans
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE AccountBranches;
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+
+
+desc Branches;
+
+alter table branches add primary key (BranchID);
+
+alter table transactions add BranchID int;
+
+ALTER TABLE Transactions
+ADD CONSTRAINT FK_Accounts_Branches
+FOREIGN KEY (BranchID)
+REFERENCES Branches(BranchID);
+
+alter table transactions add BranchID int;
+
